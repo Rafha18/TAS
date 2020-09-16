1e8c95d105d1f47c7981b92e8fd4dc2b8a9c798a
Rafael Quadra <rafhaqg@gmail.com>
2020-09-16 20:51:13 -0300

    Aula 16/09/2020

--- tas-estoque/src/main/java/br/edu/materdei/tas/estoque/entity/EstoqueEntity.java
+++ tas-estoque/src/main/java/br/edu/materdei/tas/estoque/entity/EstoqueEntity.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -21,7 +22,7 @@ import javax.persistence.TemporalType;
 public class EstoqueEntity {
     
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     
     @Temporal(TemporalType.DATE)
