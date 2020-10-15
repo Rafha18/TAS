e32bc0e8df961d6efa71a55009901b0ca4c87d5e
Rafael Quadra <rafhaqg@gmail.com>
2020-10-14 21:24:10 -0300

    Aula 14/10/20 Parte 01

--- tas-estoque/src/main/java/br/edu/materdei/tas/estoque/aspect/EstoqueAspect.java
+++ tas-estoque/src/main/java/br/edu/materdei/tas/estoque/aspect/EstoqueAspect.java
@@ -1,8 +1,13 @@
 package br.edu.materdei.tas.estoque.aspect;
 
+import br.edu.materdei.tas.compra.entity.CompraEntity;
+import br.edu.materdei.tas.estoque.entity.EstoqueEntity;
+import br.edu.materdei.tas.estoque.service.EstoqueService;
+import br.edu.materdei.tas.venda.entity.VendaEntity;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,8 +19,38 @@ import org.springframework.stereotype.Component;
 @Component
 public class EstoqueAspect {
     
+    @Autowired
+    private EstoqueService service;
+    
     @AfterReturning(pointcut = "execution( * br.edu.materdei.tas.compra.service.CompraService.save(..))")
     public void salvarCompra(JoinPoint joinPoint){
+        Object[] args = joinPoint.getArgs();
+        CompraEntity compra = (CompraEntity) args[0];
+        
+        compra.getItens().forEach(item -> {
+            EstoqueEntity estoque = new EstoqueEntity();
+            estoque.setProduto(item.getProduto());
+            estoque.setQtdade(item.getQtdade());
+            estoque.setHistorico("Movimento de Entrada Originado Pela Compra "+ compra.getCodigo());
+            
+            this.service.save(estoque);
+        });
+        
+    }
+    
+    @AfterReturning(pointcut = "execution( * br.edu.materdei.tas.venda.service.VendaService.save(..))")
+    public void salvarVenda(JoinPoint joinPoint){
+        Object[] args = joinPoint.getArgs();
+        VendaEntity venda = (VendaEntity) args[0];
+        
+        venda.getPedido().getItens().forEach(item -> {
+            EstoqueEntity estoque = new EstoqueEntity();
+            estoque.setProduto(item.getProduto());
+            estoque.setQtdade(item.getQtdade()*-1);
+            estoque.setHistorico("Movimento de Saida Originado Pela Venda "+ venda.getCodigo());
+            
+            this.service.save(estoque);
+        });
         
     }
     
